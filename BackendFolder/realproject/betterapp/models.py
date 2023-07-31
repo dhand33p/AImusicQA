c29765127aa99a5559251e61214a6ac901e1fe6a
ahirobe <xloafedbreadx@gmail.com>
2023-07-31 13:32:55 -0700

    yippee

---
+++ BackendFolder/realproject/betterapp/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Text(models.Model):
+    text = models.CharField(max_length = 500, blank = False)
+    time_sent = models.DateTimeField(auto_now_add = True)
+
+# Create your models here.
---
+++ BackendFolder/realproject/summarizer/summary.py
@@ -0,0 +1,28 @@
+# import library
+import openai
+
+import os
+from dotenv import load_dotenv, find_dotenv
+
+# finds and loads the .env file
+load_dotenv(find_dotenv())
+
+# call the variable from the loaded file
+openai.api_key = os.environ.get("openai_api_key")
+
+def summarize(text):
+	
+	# create prompt
+	prompt = "Reword the following content using as many words as possible: \n"
+	prompt += text
+	
+	# ping model and generate a response
+	response = openai.Completion.create(
+			engine = "text-davinci-003",
+			prompt = prompt,
+	)
+	
+	# clean up response to just the actual String value and return 
+	answer = response["choices"][0]["text"].strip()
+	return answer
+
