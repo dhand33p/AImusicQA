a6e05b1fcfe5233c8bb1d1959a8016ce7242f11f
omee121452 <omwins2@gmail.com>
2023-08-01 13:31:46 -0700

    git ignore test

--- BackendFolder/realproject/realproject/settings.py
+++ BackendFolder/realproject/realproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o&ovd!30pt6#a$fl)&g8=u9dreq39!#%r1hm64qofe+^7_8hjy'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
