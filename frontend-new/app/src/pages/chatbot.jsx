1040c58b5e8dd2bb790ee5eb20d087e001f9d353
ahirobe <xloafedbreadx@gmail.com>
2023-08-04 10:51:53 -0700

    frontend chatbot fixed

--- frontend-new/app/src/pages/chatbot.jsx
+++ frontend-new/app/src/pages/chatbot.jsx
@@ -1,23 +1,43 @@
 import { useState } from "react";
+
+const getSummary = (prompt) => {
+    const url = 'http://127.0.0.1:8000/SpotAi/';
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(
+            {
+                text: prompt
+            }
+        )
+    };
+    return fetch(url, options).then(res => res.json());
+}
 function Chatbot() {
     const [prompt, setPrompt] = useState('');
     const [result, setResult] = useState('...');
 
     const onSubmit = () => {
-        setResult('We recommend ' + prompt);
+        getSummary(prompt).then(result => {
+            setResult(result);
+        }).catch(error => {
+            setResult(error.message);
+        });
     };
 
     return (
-        <div className="container mx-auto text-section max-w-3xl mt-20 px-5">
+        <div className="container mx-auto max-w-3xl mt-20 px-5">
             <form onSubmit={e => e.preventDefault()} className="mb-10">
                 <label htmlFor="prompt" className="block text-2xl font-bold mb-3">
-                    My favourite songs are...
+                My favourite songs are...
                 </label>
                 <input 
                     type="text" 
                     name="prompt" 
                     id="prompt" 
-                    placeholder="Beep Beep Boop Boop. Type here!"
+                    placeholder="..."
                     value={prompt}
                     className="w-full mb-5 rounded bg-slate-200 p-2"
                     onChange={e => setPrompt(e.target.value)}
