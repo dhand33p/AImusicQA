bc8a1382964412b05f57abbf2183aeb4395a7749
flutebes <andil5@educbe.ca>
2023-08-09 12:08:25 -0600

    centered logo

--- frontend-new/app/src/pages/about.jsx
+++ frontend-new/app/src/pages/about.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import Header from './header'
 function About() {
     return (
-        <div><Header/>
+        <div>
+        <div><Header/></div>
         <div className='container text-section'>
             <h2>Meet the Team</h2>
         </div>
