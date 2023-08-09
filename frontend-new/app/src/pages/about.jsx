7c7bc20efe954b5c6ce3333c512f21c0ea355ca9
flutebes <andil5@educbe.ca>
2023-08-09 16:02:02 -0600

    updates andis info

--- frontend-new/app/src/pages/about.jsx
+++ frontend-new/app/src/pages/about.jsx
@@ -11,7 +11,7 @@ function About() {
             <ul>
                     <li>Backend dev Abby is a sophomore in high school and enjoys playing volleyball, drawing, and listening to music</li>
                     <li>Om is a freshman in highschool who works on the backend team and during his free time likes to hang out with friends, play guitar, and play sports.</li>
-                    <li>Andi plays flute with local orchestras and develops diagnostic tools for dementia. She enjoys rock climbing and archery.</li>
+                    <li>Andi is in the eigth grade and plays flute with local orchestras. She enjoys rock climbing and archery.</li>
                     <li>As the front-end designer for SpotAI, Orlando is an honor choir student in 2023 and a freshman in high school this year, making him one of the youngest members on the team.</li>
                     <li>Dhandeep is Dhandeep</li>
 
